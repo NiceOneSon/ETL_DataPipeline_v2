b34511ec0ea8e802a94950c7588711b1106b6768
손진영 <jinyoung-son@sonjin-yeongs-MacBook-Pro.local>
2023-04-19 20:29:42 +0900

    plugin

--- plugins/Async.py
+++ plugins/Async.py
@@ -1,7 +1,7 @@
 ###
-# UDF : User Defined Funtions
-# Class
-# 1. Extract : 비동기 방식으로 서버의 데이터를 크롤링해오는 객체 생성 및 작업 수행.(2022-04-18)
+# User Defined Funtions
+# 1. Class
+#    a. Extract : 비동기 방식으로 서버의 데이터를 크롤링해오는 객체 생성 및 작업 수행.(2022-04-18)
 ### 
 
 import os
@@ -10,56 +10,66 @@ import tempfile
 import requests
 import pandas as pd
 import threading
-import asyncio
-import aiohttp
 
-from typing import List, Tuple
+from typing import List, Tuple, Mapping, Any
 from pandas.core.api import DataFrame
+from airflow.models import BaseOperator
+from airflow.utils.context import Context, context_merge
 from concurrent.futures import ThreadPoolExecutor, as_completed
-# from airflow.providers.google.cloud.operators.gcs import GCSHook
+from airflow.providers.google.cloud.operators.gcs import GCSHook
 
-class Extract():
+class GCSAsyncExtractOperator(BaseOperator, GCSHook):
     def __init__(self,
                 execution_date: str,
                 url: str, 
+                gcp_conn_id: str,
                 bucket: str,
                 object_path: str,
-                params: List[Tuple[str, str]]
+                op_kwargs: Mapping[str, Any],
+                elements: List[Tuple[str, str]],
+                **kwargs
                 ):
+        
         """
         execution_date : When is the time to extract.
         url : Server url is needed.
+        gcp_conn_id : The connection id to connect google cloud platform 
         bucket : where is the bucket to store.
         object_path : what object name to store.
-        params : parameters to add on the url.
+        elements : elements to add on the url.
         """
+        BaseOperator.__init__(**kwargs)
+        GCSHook(gcp_conn_id=gcp_conn_id)
+        self.op_kwargs = op_kwargs
         self.executionDate = execution_date
         self.url = url
         self.bucket = bucket
         self.object = object_path
-        self.params = params
+        self.elements = elements
         self.lock = threading.Lock()
         self.num_of_thread = 3
         self.page = 0
 
-        if self.params == None:
-            logging.info('No params inserted.')
+        if self.elements == None:
+            logging.info('No elements inserted.')
         
         
 
-    def extract(self, page) -> dict:
+    def get_json(self, page: int) -> dict:
         url = self.url
         executionDate = self.executionDate
+        
+        if page == None:
+            raise ValueError('Page is required.')
 
-        for param, val in self.params:
-            url += f'&{param}={val}'
+        for element, val in self.elements:
+            url += f'&{element}={val}'
         url += f'&pageNo={page}'
         url += f'&basDt={self.executionDate}'
         result = requests.get(url)
 
         if result.status_code != 200:
             return False
-        
         if not result.content:
             return False
         
@@ -73,47 +83,46 @@ class Extract():
         
         return jsonResult
     
+    
     def makeDF(self,
                results: List[dict])-> DataFrame: 
-        
         dataframe = pd.DataFrame()
         return dataframe
 
     def StoreJson(self, 
-                  dataframe: DataFrame,
-                  bucket: str, 
-                  objectName: str)-> None:
+                  result: dict
+                  )-> None:
+        bucket = self.bucket
+        objectName = self.object
+
         with tempfile.TemporaryDirectory() as tmp_dir:
-            tmp_path = os.path.join(tmp_dir, f'tempDF_{self.page}.csv')
-            dataframe.to_csv(tmp_path, index = False)
-            gcs_hook = GCSHook(gcp_conn_id='gcp')
-            gcs_hook.upload(
+            tmp_path = os.path.join(tmp_dir, f'tempDF_{objectName}.json')
+            with open(tmp_path, 'w') as f:
+                import json
+                json.dump(result, f)
+            self.upload(
                 bucket_name = bucket,
                 object_name = objectName,
                 filename = tmp_path,
             )
             logging.info(f'The file is completely inserted.')
 
-
-    def execute(self)-> List[dict]: # return type : list[dict]
-        results = []
-        num_of_thread = self.num_of_thread
+    def thread_crawling(self):
+        return_value = {}
+        submitteds = set()
         control = True
+        num_of_thread = self.num_of_thread
         lock, page = self.lock, self.page
         logging.info('Crawling start!')
-        submitteds = set()
+        
         with ThreadPoolExecutor(max_workers=num_of_thread) as executor:
-            
             for _ in range(num_of_thread):
                 with lock:
                     page += 1
-
-                submitteds.add(executor.submit(self.extract, page))
-            
+                submitteds.add(executor.submit(self.get_json, page))
             while control:
                 with lock:
                     page += 1
-                
                 for submitted in as_completed(submitteds):
                     result = submitted.result()
                     if result == False:
@@ -121,24 +130,14 @@ class Extract():
                         break
                     with lock:
                         page += 1
-                    results.append(result)
+                    return_value.update(result)
                     submitteds.remove(submitted)
-                    submitteds.add(executor.submit(self.extract, page))
-
+                    submitteds.add(executor.submit(self.get_json, page))
         logging.info('Crawling Finished!')
-        print(result)
-        return results
+        return return_value
     
-        
-        
-# 사용 예시
-#     ext = Extract(
-#         execution_date='20230417',
-#         url = 'https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?',
-#         bucket = None,
-#         object_path=None,
-#         params=[('serviceKey', 'wKGRs4uOPfXPP1gmBMd619uXkZe0IijRF%2FosAG4iM4BUnCeA7bjNsptqe%2FUS6snti8Ugs9aTBLCImeLuXB4ecQ%3D%3D'),
-#                 ('numOfRows', '2000'),
-#                 ('resultType', 'json')]
-#     )
-#     result = ext.execute()
+
+    def execute(self, **context: Context)-> None: # return type : list[dict]
+        context_merge(context, self.op_kwargs)
+        result = self.thread_crawling()
+        self.StoreJson(result)
