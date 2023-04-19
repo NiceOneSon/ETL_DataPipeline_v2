cfc618027643dad4c129c1c511706babd8600850
손진영 <jinyoung-son@sonjin-yeongs-MacBook-Pro.local>
2023-04-19 22:50:48 +0900

    dag test

--- dags/prod/test.py
+++ dags/prod/test.py
@@ -1,32 +1,29 @@
-from airflow.operators.python import PythonOperator
-from datetime import datetime
+import datetime
+from plugins.Async import GCSAsyncExtractOperator
 from airflow import DAG
-import logging
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 
-default_args = {
+default_args = {'dag_id' : 'test_thread',
+                'catchup' : False,
                 'owner' : 'airflow',
                 'depends_on_past' : False,
                 'email_on_failure' : False,
                 'email_on_retry' : False,
                 'retries' : 2,
                 'start_date' : datetime(2023, 1, 1),
-                # 'retry_delay' : timedelta(seconds = 300),
                 'schedule_interval' : '0 0 * * *'
                 }
 
-def printHello():
-    logging.info("hello world!")
 
-with DAG(
-    dag_id = 'stock_etl_pipeline', 
-    default_args = default_args,
-    catchup=False
-    ) as dag:
-
-    HelloOperator = PythonOperator(
-        task_id = 'printHello',
-        python_callable = printHello,
-        dag = dag
+with DAG(default_args=default_args) as dag:
+    extract = GCSAsyncExtractOperator(
+        execution_date='{{ ds_nodash }}',
+        url = 'https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?',
+        bucket='kube-airflow-bucket',
+        object_path='/test',
+        elements=[('serviceKey', 'wKGRs4uOPfXPP1gmBMd619uXkZe0IijRF%2FosAG4iM4BUnCeA7bjNsptqe%2FUS6snti8Ugs9aTBLCImeLuXB4ecQ%3D%3D'),
+                ('numOfRows', '2000'),
+                ('resultType', 'json')]
     )
 
-    HelloOperator
+    extract
